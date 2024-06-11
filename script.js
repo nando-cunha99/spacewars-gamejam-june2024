5059acfad7784dfe4b05e4534002b1194b9af57d
Fernando <fernandobuenoneto@hotmail.com>
2024-06-10 21:56:43 -0300

    Added title text, create a function to show title text, prepared de constant to keep instructions text.

--- script.js
+++ script.js
@@ -10,6 +10,10 @@ background.src = './assets/backgroundSpace_01.1.png';
 let bgX = 0;
 let bgSpeed = 1;
 
+const titleText = 'Prepare-se para a batalha espacial!';
+
+const instructionText = 'Utilize as setas para movimentar sua nave, aperte espaço para atirar e enter para começar';
+
 function update (){
   bgX -= bgSpeed;
   if (bgX <= -canvas.width) {
@@ -21,6 +25,8 @@ function draw() {
   context.clearRect(0, 0, canvas.width, canvas.height);
   context.drawImage(background, bgX, 0, canvas.width, canvas.height);
   context.drawImage(background, bgX + canvas.width, 0, canvas.width, canvas.height);
+
+  drawText(titleText)
 }
 
 
@@ -30,7 +36,7 @@ const spaceship = {
   spriteY: 200,   //Where spaceship position in Y starts in sprite
   spriteWidth: 450,    //Spaceship width in sprite
   spriteHeight: 215,   //spaceship height in sprite
-  x: 40,    //Spaceship's initial position in X
+  x: 15,    //Spaceship's initial position in X
   y: 320,  //Spaceship's initial position in Y
   characterWidth: 110,
   characterHeight: 93,
@@ -44,6 +50,21 @@ const spaceship = {
     );
 
   }
+}
+
+function drawText (text) {
+  
+  context.fillStyle = "#f0f0f0";
+  context.font = '47px Orbitron'
+
+  const textMetrics = context.measureText(text);
+  const textWidth = textMetrics.width;
+
+  const textX = (canvas.width - textWidth)/2;
+
+  const textY = 80
+
+  context.fillText(text, textX, textY);
 }
 
 function loop(){
